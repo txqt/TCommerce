8fe82eafa55f2d01107e8a9c5ce439580ac102a9
T <hovanthanh12102002@gmail.com>
2024-08-05 13:35:03 +0700

    order detail, print

---
+++ src/TCommerce.Web/Controllers/OrderController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using TCommerce.Core.Interface;
+using TCommerce.Core.Models.Catalogs;
+using TCommerce.Core.Models.Common;
+using TCommerce.Core.Models.Orders;
+using TCommerce.Core.Models.Paging;
+using TCommerce.Core.Models.ViewsModel;
+using TCommerce.Services.AddressServices;
+using TCommerce.Services.OrderServices;
+using TCommerce.Services.PaymentServices;
+using TCommerce.Services.ProductServices;
+using TCommerce.Services.UrlRecordServices;
+using TCommerce.Web.Models;
+using TCommerce.Web.Services.PrepareModelServices;
+using static NodaTime.TimeZones.TzdbZone1970Location;
+
+namespace TCommerce.Web.Controllers
+{
+    public class OrderController : BaseController
+    {
+        private readonly IOrderService _orderService;
+        private readonly IUserService _userService;
+        private readonly IOrderModelService _orderModelService;
+
+        public OrderController(IOrderService orderService, IUserService userService, IOrderModelService orderModelService)
+        {
+            _orderService = orderService;
+            _userService = userService;
+            _orderModelService = orderModelService;
+        }
+
+        public async Task<IActionResult> UserOrders()
+        {
+            var model = new UserOrderListModel();
+            var customer = await _userService.GetCurrentUser();
+            var orders = await _orderService.SearchOrdersAsync(new OrderParameters()
+            {
+                UserId = customer.Id,
+            });
+            foreach (var order in orders)
+            {
+                var orderModel = new UserOrderListModel.OrderDetailsModel
+                {
+                    Id = order.Id,
+                    CreatedOn = order.CreatedOnUtc,
+                    OrderStatusEnum = order.OrderStatus,
+                    OrderStatus = order.OrderStatus.ToString(),
+                    PaymentStatus = order.PaymentStatus.ToString(),
+                };
+                orderModel.OrderTotal = order.OrderTotal.ToString();
+
+                model.Orders.Add(orderModel);
+            }
+
+            return View(model);
+        }
+        public virtual async Task<IActionResult> Details(int orderId)
+        {
+            var order = await _orderService.GetOrderByIdAsync(orderId);
+            var currentUser = await _userService.GetCurrentUser();
+
+            if (order == null || order.Deleted || currentUser.Id != order.UserId)
+                return Challenge();
+
+            var model = await _orderModelService.PrepareOrderDetailsModelAsync(order);
+
+            return View(model);
+        }
+        public virtual async Task<IActionResult> PrintOrderDetails(int orderId)
+        {
+            var order = await _orderService.GetOrderByIdAsync(orderId);
+            var currentUser = await _userService.GetCurrentUser();
+
+            if (order == null || order.Deleted || currentUser.Id != order.UserId)
+                return Challenge();
+
+            var model = await _orderModelService.PrepareOrderDetailsModelAsync(order);
+            model.PrintMode = true;
+
+            return View("Details", model);
+        }
+    }
+}
---
+++ src/TCommerce.Web/Services/PrepareModelServices/OrderModelService.cs
@@ -0,0 +1,118 @@
+﻿using TCommerce.Core.Interface;
+using TCommerce.Core.Models.Orders;
+using TCommerce.Core.Models.ViewsModel;
+using TCommerce.Web.Models;
+
+namespace TCommerce.Web.Services.PrepareModelServices
+{
+    public interface IOrderModelService
+    {
+        Task<OrderDetailsModel> PrepareOrderDetailsModelAsync(Order order);
+    }
+    public class OrderModelService : IOrderModelService
+    {
+        private readonly IOrderService _orderService;
+        private readonly IUserService _userService;
+        private readonly IPaymentService _paymentService;
+        private readonly IProductService _productService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IPictureService _pictureService;
+
+        public OrderModelService(IPictureService pictureService, IUrlRecordService urlRecordService, IProductService productService, IPaymentService paymentService, IUserService userService, IOrderService orderService)
+        {
+            _pictureService = pictureService;
+            _urlRecordService = urlRecordService;
+            _productService = productService;
+            _paymentService = paymentService;
+            _userService = userService;
+            _orderService = orderService;
+        }
+
+        public async Task<OrderDetailsModel> PrepareOrderDetailsModelAsync(Order order)
+        {
+            ArgumentNullException.ThrowIfNull(order);
+            var model = new OrderDetailsModel
+            {
+                Id = order.Id,
+                CreatedOn = order.CreatedOnUtc,
+                OrderStatus = order.OrderStatus.ToString(),
+                PaymentMethod = order.PaymentStatus.ToString(),
+            };
+
+            //payment method
+            var customer = await _userService.GetUserById(order.UserId);
+            var paymentMethod = _paymentService.GetPaymentMethodBySystemName(order.PaymentMethodSystemName);
+            model.PaymentMethod = paymentMethod.Name;
+
+
+            //discount (applied to order total)
+            model.OrderTotalDiscount = order.OrderDiscount.ToString();
+            model.OrderTotalDiscountValue = order.OrderDiscount;
+
+            model.OrderSubtotal = order.OrderSubtotalInclTax.ToString();
+            model.OrderSubtotalValue = order.OrderSubtotalInclTax;
+
+            //total
+            model.OrderTotal = order.OrderTotal.ToString();
+            model.OrderTotalValue = order.OrderTotal;
+
+            //order notes
+            foreach (var orderNote in (await _orderService.GetOrderNotesByOrderIdAsync(order.Id, true))
+                     .OrderByDescending(on => on.CreatedOnUtc)
+                     .ToList())
+            {
+                model.OrderNotes.Add(new OrderDetailsModel.OrderNote
+                {
+                    Id = orderNote.Id,
+                    Note = orderNote.Note,
+                    CreatedOn = orderNote.CreatedOnUtc,
+                });
+            }
+
+            //purchased products
+            model.ShowSku = true;
+            model.ShowProductThumbnail = true;
+
+            var orderItems = await _orderService.GetOrderItemsByOrderIdAsync(order.Id);
+
+            foreach (var orderItem in orderItems)
+            {
+                var product = await _productService.GetByIdAsync(orderItem.ProductId);
+
+                var orderItemModel = new OrderDetailsModel.OrderItemModel
+                {
+                    Id = orderItem.Id,
+                    Sku = product.Sku,
+                    ProductId = product.Id,
+                    ProductName = product.Name,
+                    ProductSeName = await _urlRecordService.GetSeNameAsync(product),
+                    Price = orderItem.Price.ToString(),
+                    PriceValue = orderItem.Price,
+                    Quantity = orderItem.Quantity,
+                    AttributeInfo = orderItem.AttributeInfo,
+                    SubTotal = (orderItem.Quantity * orderItem.Price).ToString(),
+                    SubTotalValue = (orderItem.Quantity * orderItem.Price)
+                };
+                model.Items.Add(orderItemModel);
+
+                var allPictureMappings = await _productService.GetProductPicturesByProductIdAsync(orderItem.ProductId);
+                if (allPictureMappings.Any())
+                {
+                    var pictureId = allPictureMappings.FirstOrDefault().PictureId;
+                    if (pictureId > 0)
+                    {
+                        var picture = await _pictureService.GetPictureByIdAsync(pictureId);
+                        orderItemModel.Picture = new PictureModel()
+                        {
+                            TitleAttribute = picture.TitleAttribute,
+                            AltAttribute = picture.AltAttribute,
+                            ImageUrl = picture.UrlPath
+                        };
+                    }
+                }
+            }
+
+            return model;
+        }
+    }
+}
